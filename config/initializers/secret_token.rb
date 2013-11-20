13563389540fc76d9e6f94cdaee6778145f05077
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-11-20 10:11:50 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyHomes::Application.config.secret_key_base = '754d3a9df2f105c548acecd1b684dff841c3f91efc8571f7998d4f2a20e4c88ed794457ae62c43c406f8cb73d8c84ccb92652e6a185798fce4d83c266be6ed51'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyHomes::Application.config.session_store :cookie_store, key: '_MyHomes_session'
