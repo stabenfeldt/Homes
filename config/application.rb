038718c0da5685d5ebac8a179271f670bdde7f5b
Martin Stabenfeldt <martin@stabenfeldt.net>
2013-11-20 10:29:39 +0100

    Updated config.generators

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,24 @@ Bundler.require(:default, Rails.env)
 
 module MyHomes
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.fixture_replacement :fabrication
+      g.template_engine     :slim
+      g.test_framework      :rspec, 
+        views: false,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+    end
+
+    Fabrication.configure do |config|
+      fabricator_dir = "data/fabricators"
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
